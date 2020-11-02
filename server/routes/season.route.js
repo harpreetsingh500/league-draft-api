231fcaa53bbdaa704a9c544d28869a6b1bc2c6f3
Your Name <harpeetsingh500@gmail.com>
2020-11-02 17:26:02 -0600

    sorting the season matches by date when getting a season

--- server/routes/season.route.js
+++ server/routes/season.route.js
@@ -170,7 +170,13 @@ async function getSeason(req, res) {
   let players = await playerCtrl.getAllPlayers(seasonId);
   let matches = await seasonCtrl.getAllMatches(seasonId);
 
-  matches = matches.map(match => match.toObject());
+  matches = matches.map(match => match.toObject()).sort((match1, match2) => {
+    const match1Date = new Date(`${match1.matchDate} ${match1.matchTime} ${match1.timeZone}`);
+    const match2Date = new Date(`${match2.matchDate} ${match2.matchTime} ${match2.timeZone}`)
+
+    return match1Date - match2Date;
+  });
+
   players = players.map(player => player.toObject());
 
   players.forEach(player => {
